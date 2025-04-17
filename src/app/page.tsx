d458efce86b7b2fa529c8c9fa07133712bf96a58
aimanshafi <shafiaiman88@gmail.com>
2025-04-17 23:24:48 +0600

    Create: Hero component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
 import Explore from "@/components/home/explore-section/Explore";
+import Hero from "@/components/home/hero/Hero";
 
 export default function Home() {
   return (
     <>
+      <Hero />
       <Explore />
     </>
   );
