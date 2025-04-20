8c0ad63459fadef2400cd335ce3d95659f98d5ff
Mosarrof-Coding <tinaberry112188@gmail.com>
2025-04-20 15:47:54 +0600

    Create: New Listing Section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import Explore from "@/components/home/explore-section/Explore";
 import Hero from "@/components/home/hero/Hero";
+import { Listing } from "@/components/home/listing/Listing";
 
 export default function Home() {
   return (
     <>
       <Hero />
       <Explore />
+      <Listing />
     </>
   );
 }
