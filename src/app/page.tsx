7b0484410260020f65ac32f56f65ef7684e0c61e
Mosarrof-Coding <tinaberry112188@gmail.com>
2025-04-20 16:08:11 +0600

    Update: Export Syntax of Listing Components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 import Explore from "@/components/home/explore-section/Explore";
 import Hero from "@/components/home/hero/Hero";
-import { Listing } from "@/components/home/listing/Listing";
+import Listing from "@/components/home/listing/Listing";
 
 export default function Home() {
   return (
