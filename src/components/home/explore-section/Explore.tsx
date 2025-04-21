9df2f171f085f08062485b37c6e11377871510f9
Israt Zahan <isratzahanpritha@gmail.com>
2025-04-21 23:19:12 +0600

    Latest update animation

--- src/components/home/explore-section/Explore.tsx
+++ src/components/home/explore-section/Explore.tsx
@@ -6,6 +6,7 @@ import dream3 from "@/../public/explore/dream3.svg";
 import dream4 from "@/../public/explore/dream4.svg";
 import dreamhouseimage from "@/../public/explore/dreamhouseimage.png";
 import Background from "@/../public/explore/Background.png";
+import FadeIn from "@/components/animations/FadeIn";
 
 const Explore = () => {
   return (
@@ -14,86 +15,118 @@ const Explore = () => {
         <div className="flex lg:flex-row flex-col justify-between gap-2.5 mx-auto max-w-[1088px]">
           <div className="flex sm:flex-row flex-col justify-between gap-2 mx-auto max-w-fit dreamImage">
             <div className="dreamHeadingr">
-              <h2 className="mb-4 font-medium text-[26px] text-foreground sm:text-[32px] lg:text-[40px] leading-[40px] sm:leading-[46px] lg:leading-[48px] tracking-[-1px] sm:tracking-[-2px]">
-                Explore & Find <br className="" /> your{" "}
-                <span className="text-primary">
-                  Dream
-                  <br className="hidden sm:block" /> home
-                </span>
-              </h2>
+              <FadeIn delay={0.2}>
+                <h2 className="mb-4 font-medium text-[26px] text-foreground sm:text-[32px] lg:text-[40px] leading-[40px] sm:leading-[46px] lg:leading-[48px] tracking-[-1px] sm:tracking-[-2px]">
+                  Explore & Find <br className="" /> your{" "}
+                  <span className="text-primary">
+                    Dream
+                    <br className="hidden sm:block" /> home
+                  </span>
+                </h2>{" "}
+              </FadeIn>
               <div className="relative w-fit">
-                <Image
-                  src={dreamhouseimage}
-                  alt="dreamhouseimage"
-                  className=""
-                />
+                <FadeIn delay={0.8}>
+                  <Image
+                    src={dreamhouseimage}
+                    alt="dreamhouseimage"
+                    className=""
+                  />
+                </FadeIn>
                 <div className="top-full left-0 absolute bg-card px-[19px] py-3 rounded-[8px] sm:rounded-[16px] -translate-y-1/2">
                   <div className="flex">
                     <div className="shadow-[0px_8px_12px_rgba(0,0,0,0.08)] border-[2px] border-white rounded-full w-fit">
-                      <Image src={dream1} alt="dream1" />
+                      <FadeIn delay={0.8}>
+                        <Image src={dream1} alt="dream1" />
+                      </FadeIn>
                     </div>
                     <div className="shadow-[0px_8px_12px_rgba(0,0,0,0.08)] border-[2px] border-white rounded-full w-fit">
-                      <Image src={dream2} alt="dream2" />
+                      <FadeIn delay={0.8}>
+                        <Image src={dream2} alt="dream2" />
+                      </FadeIn>
                     </div>
                     <div className="shadow-[0px_8px_12px_rgba(0,0,0,0.08)] border-[2px] border-white rounded-full w-fit">
-                      <Image src={dream3} alt="dream3" />
+                      <FadeIn delay={0.8}>
+                        <Image src={dream3} alt="dream3" />
+                      </FadeIn>
                     </div>
                     <div className="shadow-[0px_8px_12px_rgba(0,0,0,0.08)] border-[2px] border-white rounded-full w-fit">
-                      <Image src={dream4} alt="dream4" />
+                      <FadeIn delay={0.8}>
+                        <Image src={dream4} alt="dream4" />
+                      </FadeIn>
                     </div>
                   </div>
-                  <h6 className="mt-1 sm:mt-2 font-normal text-[14px] text-center leading-[19px] sm:leading-[21px]">
-                    300+ Happy Client
-                  </h6>
+                  <FadeIn delay={0.2}>
+                    <h6 className="mt-1 sm:mt-2 font-normal text-[14px] text-center leading-[19px] sm:leading-[21px]">
+                      300+ Happy Client
+                    </h6>
+                  </FadeIn>
                 </div>
               </div>
             </div>
             <div className="dreamImage">
-              <Image src={Background} alt="Background" className="" />
+              {" "}
+              <FadeIn delay={0.8}>
+                <Image src={Background} alt="Background" className="" />
+              </FadeIn>
             </div>
           </div>
           {/* text */}
           <div className="bg-card mx-auto sm:mt-12 lg:mt-0 p-3 sm:p-4 lg:p-6 rounded-[12px] lg:rounded-[16px] max-w-[520px] sm:max-w-[644px] lg:max-w-[429px] content shrink-3">
-            <p className="mb-16 sm:mb-20 lg:mb-22 font-normal text-[16px] leading-[22px] sm:leading-[24px]">
-              “We believe there&#39;s a perfect home for everybody, no matter
-              the budget. That&#39;s why we always find the best homes for your
-              budget.”
-            </p>
+            <FadeIn delay={0.5}>
+              {" "}
+              <p className="mb-16 sm:mb-20 lg:mb-22 font-normal text-[16px] leading-[22px] sm:leading-[24px]">
+                “We believe there&#39;s a perfect home for everybody, no matter
+                the budget. That&#39;s why we always find the best homes for
+                your budget.”
+              </p>
+            </FadeIn>
             <div className="flex gap-12 lg:gap-19 py-4 lg:py-6 border-y border-black/4">
               <div>
-                <h2 className="mb-1 font-medium text-[26px] md:text-[32px] lg:text-[40px] leading-[44px] sm:leading-[46px] lg:leading-[48px] tracking-[-1px] sm:tracking-[-2px]">
-                  $200M+
-                </h2>
-                <h6 className="font-semibold text-[14px] leading-[19px] sm:leading-[21px]">
-                  Sold in Real Estate
-                </h6>
+                <FadeIn delay={0.2}>
+                  <h2 className="mb-1 font-medium text-[26px] md:text-[32px] lg:text-[40px] leading-[44px] sm:leading-[46px] lg:leading-[48px] tracking-[-1px] sm:tracking-[-2px]">
+                    $200M+
+                  </h2>
+                </FadeIn>
+                <FadeIn delay={0.2}>
+                  <h6 className="font-semibold text-[14px] leading-[19px] sm:leading-[21px]">
+                    Sold in Real Estate
+                  </h6>
+                </FadeIn>
               </div>
               <div>
-                <h2 className="mb-1 font-medium text-[26px] md:text-[32px] lg:text-[40px] leading-[44px] sm:leading-[46px] lg:leading-[48px] tracking-[-1px] sm:tracking-[-2px]">
-                  100+
-                </h2>
-                <p className="font-semibold text-[14px] leading-[19px] sm:leading-[21px]">
-                  Property Sold
-                </p>
+                <FadeIn delay={0.2}>
+                  <h2 className="mb-1 font-medium text-[26px] md:text-[32px] lg:text-[40px] leading-[44px] sm:leading-[46px] lg:leading-[48px] tracking-[-1px] sm:tracking-[-2px]">
+                    100+
+                  </h2>
+                </FadeIn>
+                <FadeIn delay={0.5}>
+                  <p className="font-semibold text-[14px] leading-[19px] sm:leading-[21px]">
+                    Property Sold
+                  </p>
+                </FadeIn>
               </div>
             </div>
             <ul className="flex flex-col gap-1 sm:gap-2 my-4 md:my-6">
-              <li className="flex items-center gap-1.5 sm:gap-2.5">
-                <span className="inline-block bg-foreground rounded-full w-[10px] h-[10px]"></span>
-                Over 100k+ property & update regularly
-              </li>
-              <li className="flex items-center gap-1.5 sm:gap-2.5">
-                <span className="inline-block bg-foreground rounded-full w-[10px] h-[10px]"></span>
-                Expert agent consultation
-              </li>
-              <li className="flex items-center gap-1.5 sm:gap-2.5">
-                <span className="inline-block bg-foreground rounded-full w-[10px] h-[10px]"></span>
-                Loan & low Interest facility
-              </li>
-            </ul>
-            <button className="bg-foreground hover:bg-primary rounded-md text-background dark:hover:text-foreground cursor-pointer btn">
-              Browse Listings
-            </button>
+              <FadeIn delay={0.8}>
+                <li className="flex items-center gap-1.5 sm:gap-2.5">
+                  <span className="inline-block bg-foreground rounded-full w-[10px] h-[10px]"></span>
+                  Over 100k+ property & update regularly
+                </li>
+                <li className="flex items-center gap-1.5 sm:gap-2.5">
+                  <span className="inline-block bg-foreground rounded-full w-[10px] h-[10px]"></span>
+                  Expert agent consultation
+                </li>
+                <li className="flex items-center gap-1.5 sm:gap-2.5">
+                  <span className="inline-block bg-foreground rounded-full w-[10px] h-[10px]"></span>
+                  Loan & low Interest facility
+                </li>
+              </FadeIn>
+            </ul>{" "}
+            <FadeIn delay={0.8}>
+              <button className="bg-foreground hover:bg-primary rounded-md text-background dark:hover:text-foreground cursor-pointer btn">
+                Browse Listings
+              </button>{" "}
+            </FadeIn>
           </div>
         </div>
       </div>
--- src/components/home/listing/Listing.tsx
+++ src/components/home/listing/Listing.tsx
@@ -1,17 +1,22 @@
 import React from "react";
 import Cards from "./Cards";
+import FadeIn from "@/components/animations/FadeIn";
 
 const Listing = () => {
   return (
     <section className="py-16 sm:py-20 md:py-24 lg:py-30">
       <div className="mx-auto px-4 max-w-[1232px]">
         <div className="text-center listHeading">
-          <h2 className="mb-1 sm:mb-2 lg:mb-4 text-destructive leading-[44px] sm:leading-[46px] lg:leading-[48px] tracking-[-1px] sm:tracking-[-2px]">
-            <span>New Listing</span>
-          </h2>
-          <p className="font-normal text-[16px] leading-[22px] sm:leading-[24px]">
-            Explore latest & featured properties for sale.
-          </p>
+          <FadeIn delay={0.2}>
+            <h2 className="mb-1 sm:mb-2 lg:mb-4 text-primary leading-[44px] sm:leading-[46px] lg:leading-[48px] tracking-[-1px] sm:tracking-[-2px]">
+              <span>New Listing</span>
+            </h2>{" "}
+          </FadeIn>{" "}
+          <FadeIn delay={0.5}>
+            <p className="font-normal text-[16px] leading-[22px] sm:leading-[24px]">
+              Explore latest & featured properties for sale.
+            </p>{" "}
+          </FadeIn>
         </div>
         <div className="gap-4 lg:gap-6 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 mt-14 sm:mt-18 md:mt-20 lg:mt-26">
           <Cards />
