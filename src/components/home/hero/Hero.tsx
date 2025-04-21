14856a1cad2fe7ffee7a0ec1f1f99ef05108d837
aimanshafi <shafiaiman88@gmail.com>
2025-04-21 10:51:38 +0600

    Update: Framer Motion Animations Testing

--- src/components/home/hero/Hero.tsx
+++ src/components/home/hero/Hero.tsx
@@ -1,5 +1,4 @@
-"use client";
-
+import FadeIn from "@/components/animations/FadeIn";
 import Image from "next/image";
 import React from "react";
 
@@ -20,31 +19,37 @@ export default function HeroSection() {
             />
           </div>
 
-          <h1 className="h1">
-            Find the <span className="text-primary">perfect</span>
-            <br />
-            <span className="text-primary">property</span> with{" "}
-            <span className="text-foreground">Benfort</span>
-          </h1>
-
+          <FadeIn delay={0.2}>
+            <h1 className="h1">
+              Find the <span className="text-primary">perfect</span>
+              <br />
+              <span className="text-primary">property</span> with{" "}
+              <span className="text-foreground">Benfort</span>
+            </h1>
+          </FadeIn>
           {/* ✅ Desktop: Logo পাশে */}
-          <div className="hidden md:block">
-            <Image
-              src="/5.png"
-              alt="Hero Icon"
-              width={107}
-              height={56}
-              className="transition-transform md:-translate-x-10 md:-translate-y-10 duration-500 transform"
-            />
-          </div>
+          <FadeIn delay={0.8}>
+            <div className="hidden md:block">
+              <Image
+                src="/5.png"
+                alt="Hero Icon"
+                width={107}
+                height={56}
+                className="transition-transform md:-translate-x-10 md:-translate-y-10 duration-500 transform"
+              />
+            </div>
+          </FadeIn>
         </div>
 
         {/* Subtext */}
-        <p className="mt-[26px] text-secondary-foreground para">
-          We believe there’s a perfect home for everybody, no matter the budget.
-          <br />
-          That’s why we always find the best homes for your budget.
-        </p>
+        <FadeIn delay={0.5}>
+          <p className="mt-[26px] text-secondary-foreground para">
+            We believe there’s a perfect home for everybody, no matter the
+            budget.
+            <br />
+            That’s why we always find the best homes for your budget.
+          </p>
+        </FadeIn>
 
         {/* Images Section */}
         <div className="flex lg:flex-row flex-col justify-center items-center gap-6 mx-auto py-[88px]">
