ca27a24a5f01b07a9efa32326113f1e347d3754e
Mosarrof-Coding <tinaberry112188@gmail.com>
2025-04-22 21:38:46 +0600

    Create: "benfort-home-cta-contact" name as get-in-touch

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import Explore from "@/components/home/explore-section/Explore";
 import Featured from "@/components/home/featured/Featured";
+import GetInTouch from "@/components/home/get-in-touch/GetInTouch";
 import GetMore from "@/components/home/get-more/GetMore";
 import Hero from "@/components/home/hero/Hero";
 import Listing from "@/components/home/listing/Listing";
@@ -18,6 +19,7 @@ export default function Home() {
       <GetMore />
       <OurAgentsSection />
       <SubscribeSection />
+      <GetInTouch />
     </>
   );
 }
